00f5c6a2ab4a1e2b9d2379063adc4970f26170c0
Claire Melville <cmelville@lcs.on.ca>
2022-02-15 11:13:59 -0500

    Initial Commit

---
+++ Boiling Water/Boiling_WaterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Boiling_WaterApp.swift
+//  Boiling Water
+//
+//  Created by Claire Coding Account on 2022-02-15.
+//
+
+import SwiftUI
+
+@main
+struct Boiling_WaterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
